7e7c35618c6c3981ab27fe6b86733d119a56fef7
Philippe CHARRIERE <k33g@github.com>
2016-09-08 14:53:12 +0200

    🌺 Gardening ... a lot

--- scripts/bobot.js
+++ scripts/bobot.js
@@ -4,15 +4,29 @@
 
 "use strict";
 // TODO: add config file
-const VerEx = require('verbal-expressions');
+// TODO: add some functional "goodies"
 
 const GitHubClient = require('./GitHubClient.js').GitHubClient;
 let githubCli = new GitHubClient({
   baseUri: "http://github.at.home/api/v3",
-  token:process.env.TOKEN_GHE_27_K33G
+  token:process.env.TOKEN_GHE_27_BOBTHEBOT
 })
 
-const words = require('./words.js')
+//  token:process.env.TOKEN_GHE_27_K33G
+
+String.prototype.equals = function(x) { return x == this; }
+let tokenize = cmd => {
+  let tokenizedCmd = cmd.split(" ").filter(item => item !== "");
+  //tokenizedCmd.forEach(item => item.prototype.equals = (x) => x == item);
+  tokenizedCmd.first = () => tokenizedCmd[0];
+  tokenizedCmd.second = () => tokenizedCmd[1];
+  tokenizedCmd.third = () => tokenizedCmd[2];
+  tokenizedCmd.fourth = () => tokenizedCmd[3];
+  tokenizedCmd.fifth = () => tokenizedCmd[4];
+  tokenizedCmd.sixth = () => tokenizedCmd[5];
+
+  return tokenizedCmd;
+}
 
 
 module.exports =  (robot) =>  {
@@ -30,22 +44,40 @@ module.exports =  (robot) =>  {
   - "id":"C278CLABB","name":"random"
   */
 
+  // http://localhost:8082/hey/hubot
+  robot.router.get('/hey/hubot', (req, res) => {
+    robot.messageRoom('C278BQHRV', 'Somebody try to :wave: me, please :hourglass_flowing_sand:');
+    res.send({});
+  });
+
+  // listen to hook(s)
+  robot.router.post('/hey/hubot', (req, res) => {
+    robot.messageRoom('C279GEGER', [
+      `:zap: Event: ${req.headers['x-github-event']}\n`,
+      `:panda_face: sender: ${req.body.sender.login} | ${req.body.sender.html_url}\n`,
+      `:house: organization: ${req.body.organization.login} | ${req.body.organization.url}\n`,
+      `:package: repository: ${req.body.repository.name} | ${req.body.repository.html_url}\n`,
+      `:page_facing_up: head_commit: ${req.body.head_commit.message}\n`,
+      `${req.body.head_commit.url}\n`
+    ].join(""));
+    res.send({});
+  });
+
+
   robot.hear(/bob/, (res) => {
     let cmd = res.envelope.message.text;
-    //console.log(res.envelope.message.text)
-    //let words = res.envelope.message.text.split(" ")
+    let tokenizedCmd = tokenize(cmd);
+
     try {
-      if (cmd=="bob") {
+      if (tokenizedCmd.first().equals("bob") && tokenizedCmd.length == 1) {
         res.send("What?");
       }
-      if (words.testIfHelpMeKeyWord(cmd)) {
+      if (tokenizedCmd.second().equals("helpme")) {
         res.send("=== HELP ===");
       }
       // get informations about a user
-      if (words.testIfUserKeyWord(cmd)) {
-        let user = words.getStringAfterUser(cmd)
-
-        //console.log("-->",user)
+      if (tokenizedCmd.second().equals("user")) {
+        let user = tokenizedCmd.third()
 
         githubCli.fetchUser({handle:user}).then(user => {
           //console.log(user);
@@ -66,8 +98,9 @@ module.exports =  (robot) =>  {
         })
       }
       // suspend a user
-      if (words.testIfSuspendKeyWord(cmd)) {
-        let user = words.getStringAfterSuspend(cmd)
+      if (tokenizedCmd.second().equals("suspend")) {
+        let user = tokenizedCmd.third();
+
         githubCli.suspendUser({handle:user}).then(data => {
           console.log(data);
           res.send(`${user} suspended!`);
@@ -77,8 +110,9 @@ module.exports =  (robot) =>  {
         })
       }
       // unsuspend a user
-      if (words.testIfUnsuspendKeyWord(cmd)) {
-        let user = words.getStringAfterUnsuspend(cmd)
+      if (tokenizedCmd.second().equals("unsuspend")) {
+        let user = tokenizedCmd.third();
+
         githubCli.unsuspendUser({handle:user}).then(data => {
           console.log(data);
           res.send(`${user} unsuspended!`);
@@ -89,8 +123,9 @@ module.exports =  (robot) =>  {
       }
 
       // create a repository
-      if (words.testIfCreateRepoKeyWords(cmd)) {
-        let repoName = words.getStringAfterRepo(cmd)
+      // bob create repo something
+      if (tokenizedCmd.second().equals("create") && tokenizedCmd.third().equals("repo")) {
+        let repoName = tokenizedCmd.fourth()
 
         githubCli.createPublicRepository({name:repoName, description:"TBD"})
           .then(repo => {
@@ -103,23 +138,25 @@ module.exports =  (robot) =>  {
           })
 
       }
+      // bob create orga repo something ACME
+      if (
+        tokenizedCmd.second().equals("create") &&
+        tokenizedCmd.third().equals("orga") &&
+        tokenizedCmd.fourth().equals("repo")) {
 
-      if (words.testIfCreateOrgaRepoKeyWordsAnd2WordsAfter(cmd)) {
-        let repoName = words.getRepoNameAndOrgaName(cmd)[0]
-        let repoOrga = words.getRepoNameAndOrgaName(cmd)[1]
+
+        let repoName = tokenizedCmd.fifth()
+        let repoOrga = tokenizedCmd.sixth()
 
         githubCli.createPublicOrganizationRepository({name:repoName, description:"TBD", organization:repoOrga})
           .then(repo => {
-            //console.log(repo)
             res.send(repo.html_url);
           }).catch(error => {
             res.send("No way!!! :rage:");
             console.log("error", error)
-            //res.send("Huston? We've got a problem");
           })
 
       }
-
 
     } catch(err) {
       res.send("Huston? We've got a problem :scream:");
